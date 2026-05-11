64618e47624076e6e15e4c394b620aeb830d2b3b
Jeromekessie <Jerome.kessie@gmail.com>
2026-05-11 10:31:57 -0400

    student pri master

---
+++ src/studentlist/Student.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author JeromeK
+ */
+public class Student {
+    private String name; //define all varible to pravate // ensapsulation wrapped with getter and setter 1st prinpacle
+    private int sid;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+}
